1618d15c3e8ae08b0a321917cb382a769979ae2e
Nitin <canvas.nv@gmail.com>
2019-03-20 14:08:20 +0530

    bottom nav working

---
+++ app/src/main/java/com/nitin/weather/ui/settings/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.nitin.weather.ui.settings
+
+import androidx.fragment.app.Fragment
+
+//TODO make it preference fragment
+class SettingsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/nitin/weather/ui/weather/future/detail/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.nitin.weather.ui.weather.future.detail
+
+import androidx.lifecycle.ViewModel;
+
+class FutureDetailWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
